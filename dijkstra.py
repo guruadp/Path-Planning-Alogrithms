796d420693c2111fcc76005ddfc811e578eaff79
guruadp <gurunandhanadp@gmail.com>
2022-02-11 16:06:15 +0530

    Completed

--- dijkstra.py
+++ dijkstra.py
@@ -2,7 +2,35 @@ import sys
 from heapq import heapify, heappush
 
 def dijkstra(graph, src, dest):
-    pass
+    inf = sys.maxsize
+    node_data = {
+        'A':{'cost':inf, 'pred':[]},
+        'B':{'cost':inf, 'pred':[]},
+        'C':{'cost':inf, 'pred':[]},
+        'D':{'cost':inf, 'pred':[]},
+        'E':{'cost':inf, 'pred':[]},
+        'F':{'cost':inf, 'pred':[]}
+    }
+    node_data[src]['cost'] = 0
+    visited = []
+    temp = src
+    for i in range(5):
+        if temp not in visited:
+            visited.append(temp)
+            min_heap = []
+            for j in graph[temp]:
+                if j not in visited:
+                    cost = node_data[temp]['cost'] + graph[temp][j]
+                    if cost < node_data[j]['cost']:
+                        node_data[j]['cost'] = cost
+                        node_data[j]['pred'] = node_data[temp]['pred'] + list(temp)
+                    heappush(min_heap, (node_data[j]['cost'],j))
+        heapify(min_heap)
+        temp = min_heap[0][1]    
+    print("Shortest Distance: " + str(node_data[dest]['cost']))
+    print("Shortest Path: " + str(node_data[dest]['pred'] + list(dest)))
+
+
 
 if __name__ == "__main__":
     graph = {
